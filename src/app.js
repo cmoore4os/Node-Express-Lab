c40d21df456c86c7dde28e44dbe387a4f55162bd
cmoore4os <cm4glos@gmail.com>
2017-12-20 19:31:12 -0500

    Consuella Moore -- Initial commit -- finished server.get and server.post

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 const { server } = require('./server.js');
 
-server.listen(3000);
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running  port ${port}`);
+});
+
--- src/server.js
+++ src/server.js
@@ -1,16 +1,43 @@
-const bodyParser = require('body-parser');
 const express = require('express');
+const bodyParser = require('body-parser');
 
+// globals
+const server = express();
 const STATUS_USER_ERROR = 422;
 
-// This array of posts persists in memory across requests. Feel free
-// to change this to a let binding if you need to reassign it.
+// give each post a id number
+let idNum = 0;
+// post = {title: "Post Title", contents: "Post Contents" id: idNum}
 const posts = [];
 
-const server = express();
+// Use
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+
+// Get
+server.get('/api/posts', (req, res) => {
+  console.log(req.query);
+  res.status(200).json(posts);
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const post = posts.find(f => f.id === req.params.id);
+  res.status(200).json(post);
+});
+// Post
+server.post('/api/posts', (req, res) => {
+  console.log(req.body);
+  const post = req.body;
+  post.id = idNum++;
+  posts.push(post);
+  res.status(201).json(posts);
+});
+// Put
+
+// Delete
+
+// server.listen(port, () => console.log(`Server running  port ${port}`));
 
 module.exports = { posts, server };
